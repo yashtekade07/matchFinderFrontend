148c2815e389f16d83fe92563678d66b31d3b7d8
Deceit <yashtekade07@gmail.com>
2023-07-20 16:18:16 +0530

    final

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,7 +12,7 @@ const Dashboard = () => {
 
     const userId = cookies.UserId
 
-    const server="https://matchfinder.onrender.com/dashboard"
+    const server="https://matchfinder.onrender.com/"
     const getUser = async () => {
         try {
             const response = await axios.get(`${server}/user`, {
